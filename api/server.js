8beed1631a0e71ac18c40389ec8e587de55c594e
Nikolai Lvov <nikolailvov@Nikolais-MacBook-Pro.local>
2022-06-15 19:20:54 -0700

    Passed all tests

--- api/server.js
+++ api/server.js
@@ -1,2 +1,22 @@
 // implement your server here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
 // require your posts router and connect it here
+
+const postsRouter = require('./posts/posts-router');
+
+server.use('/api/posts', postsRouter);
+
+// OTHER ENDPOINTS
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Post API</h>
+      <p>Welcome to the Post API</p>
+    `);
+  });
+
+module.exports = server;
